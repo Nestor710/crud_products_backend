ebdcb2ac683530ad6867fa9a7d60be80acb9877f
nestor <nestorlima710@gmail.com>
2021-08-08 15:16:12 -0500

    p

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,11 +37,11 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
         
         'api' => [
             'throttle:api',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\Cors::class
         ],
     ];
